b49916efdcfec0ddf6619b332f24f4954be9be12
Alastair Cooper <alastair.cooper@outlook.com>
2018-01-23 11:17:01 +0000

    Configured port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-
+const PORT = process.env.PORT || 5000
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -17,5 +17,4 @@ app.get('/', function(req, res) {
 // enable access to static pages
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.listen(8080);
-console.log('8080 is the magic port');
+app.listen(PORT);
