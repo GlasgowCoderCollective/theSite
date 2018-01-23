88fae81d41a63f9a50382040c172cc8f15ce6196
Alastair Cooper <alastair.cooper@outlook.com>
2018-01-23 08:04:34 +0000

    Added static route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // app.js
 // load the things we need
 var express = require('express');
+var path = require('path');
 var app = express();
 
 // set the view engine to ejs
@@ -12,6 +13,9 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
+
+// enable access to static pages
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.listen(8080);
 console.log('8080 is the magic port');
